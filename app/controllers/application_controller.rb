556c890f2be1d5cbb1ceda54326ba3290c9da65a
martinez <hugo.martinez@polytechnique.edu>
2017-01-14 13:26:54 +0100

    rollback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  skip_before_filter  :verify_authenticity_token
 
 def cors_set_access_control_headers
   headers['Access-Control-Allow-Origin'] = '*'
