f1b3495d5a1f883a13c583fcf0bcf5444797fda5
martinez <hugo.martinez@polytechnique.edu>
2017-01-14 13:18:49 +0100

    skip token app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_filter  :verify_authenticity_token
 
 def cors_set_access_control_headers
   headers['Access-Control-Allow-Origin'] = '*'
