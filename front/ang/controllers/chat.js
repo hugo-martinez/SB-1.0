3c788d8d2d83e2fbce35cbecb45d940bb274195a
vatre <trean.v@gmail.com>
2017-01-15 19:29:45 +0000

    Chat refresh is now working, bitch

--- front/ang/controllers/chat.js
+++ front/ang/controllers/chat.js
@@ -4,6 +4,11 @@
   ['$http', '$scope', 'api', 'users', '$location', '$routeParams',
   function($http, $scope, api, users, $location, $routeParams) {
     var ctrl = this;
+    ctrl.lastMessageDT = '1970-01-01T00:00:00.902Z';
+    ctrl.refreshPeriod = [2000, 5000, 10000, 30000];
+    ctrl.cycleDuration = [10000, 30000, 30000, -1];
+    ctrl.currentCountdown = ctrl.cycleDuration[0];
+    ctrl.cycleCursor = 0;
 
     $scope.checkLogin = function() {
       if (! $scope.$storage.hasOwnProperty("user")) {
@@ -25,19 +30,14 @@
       if (! $scope.hasOwnProperty("interloc")) {
         api.getConversation($scope.convId)
         .then(function(conv) {
-          console.log("Trying to fetch this fucking damn id");
-          console.log(conv);
           var interId = conv.data.user_id1 == $scope.$storage.user.id ?
             conv.data.user_id2:
             conv.data.user_id1;
           api.getUser(interId)
           .then(function(resp) {
-            console.log("Here we try to fetch the fucking interlocutor");
-            console.log(resp);
             $scope.interloc = {
               name: resp.data.first_name,
             };
-            console.log("Interlocutor : " + $scope.interloc.name);
           }, function(error) {
             console.log("Error fetching the interlocutor infos.");
             console.log(error);
@@ -49,20 +49,17 @@
           $location.path("/500");
         })
       }
-    }
+    };
 
     $scope.loadMessages = function() {
       loadInterloc();
-      console.log("Loading messages for chat");
       var prom = api.getMessages($scope.convId);
       prom.then(function(resp) {
         var rawMessages = resp.data;
         var messages = [];
-        console.log("Processing messages : ");
-        console.log(rawMessages);
         nMessages = rawMessages.length;
         for (var i = 0; i < nMessages; i++) {
-          htmlClass = (rawMessages[i].user_id == $scope.$storage.user.id) ?
+          var htmlClass = (rawMessages[i].user_id == $scope.$storage.user.id) ?
             "sent" :
             "received";
           var message = {
@@ -70,6 +67,9 @@
             dateTime: rawMessages[i].time,
             content: rawMessages[i].msg,
           };
+          if (message.dateTime > ctrl.lastMessageDT) {
+            ctrl.lastMessageDT = message.dateTime;
+          }
           messages.push(message);
         }
         messages.sort(function(m1, m2) {
@@ -78,14 +78,52 @@
           return 0;
         });
         $scope.messages = messages;
-        console.log("Messages loaded");
-        console.log($scope.messages);
       }, function(error) {
         console.log("Error fetching messages for chat :");
         console.log(error);
         $location.path("/500");
       })
-    }
+      ctrl.callRefresh();
+    };
 
+    // Use callRefresh to launch automatic refreshing
+    ctrl.refresh = function() {
+      var prom = api.getMessages($scope.convId);
+      prom.then(function(resp) {
+        var rawMessages = resp.data;
+        var nMessages = rawMessages.length;
+        for (var j = 0; j < nMessages; j++) {
+          if (rawMessages[j].time > ctrl.lastMessageDT) {
+            var htmlClass = (rawMessages[j].user_id == $scope.$storage.user.id) ?
+              "sent" :
+              "received";
+            var message = {
+              class: htmlClass,
+              dateTime: rawMessages[j].time,
+              content: rawMessages[j].msg,
+            };
+            ctrl.lastMessageDT = message.dateTime;
+            $scope.messages.push(message);
+          }
+        }
+      }, function(error) {
+        console.log("Error while trying to refresh messages from the user chat.");
+        console.log(error);
+        $location.path('/500');
+      });
+    };
+
+    ctrl.callRefresh = function() {
+      var refreshPeriod = ctrl.refreshPeriod[ctrl.cycleCursor];
+      ctrl.refresh();
+      setTimeout(function() {
+        ctrl.callRefresh();
+      }, refreshPeriod);
+      var cycleDuration = ctrl.cycleDuration[ctrl.cycleCursor];
+      if (ctrl.currentCountdown <= 0 && cycleDuration != -1) {
+        ctrl.cycleCursor ++;
+        ctrl.currentCountdown = ctrl.cycleDuration[ctrl.cycleCursor];
+      }
+    };
   }]);
 })();
